8342885d927483e75dd1d28df2ff0f9d500287af
vigman <1319284583@qq.com>
2022-06-02 22:59:19 +0800

    算法概述

--- STL/algorithm.cpp
+++ STL/algorithm.cpp
@@ -107,4 +107,231 @@ using namespace std;
 algTest02.cpp
 */
 
+/*删除算法
+- 删除一个容器里的某些元素
+- 删除不会使容器里的元素减少
+    - 将所有应该被删除的元素看作空位子
+    - 用留下的元素从后往前移，依次去填空位子
+    - 元素往前移后，他原来的位置也就算是空位子
+    - 也应由后面的留下的元素来填上
+    - 最后，没有被填上的空位子，维持其原来的值不变
+- 删除算法不应作用于关联容器
+- 常见删除算法
+    - remove 删除区间中等于某个值得元素
+    - remove_if 删除区间中满足某种条件的元素
+    - remove_copy 拷贝区间到另一个区间 等于某个值的元素不拷贝
+    - remove_copy_if 拷贝区间到另一个区间 符合某种条件的元素不拷贝
+    - unique 删除区间中连续相等的元素 只留下一个(可自定义比较器)
+        tempalte<class FwdIt>
+        FwdIt unique(FwdIt first,FwdIt last);
+        用 == 比较是否相等（自定义）
+        tempalte<class FwdIt,class Pred>
+        FwdIt unique(FwdIt first,FwdIt last,Pred pr);
+        用pr(x,y)为true说明x和y相等
+        对[first,last)这个序列中连续相等的元素，只留下第一个
+        返回值是迭代器，指向元素删除后的区间的最后一个元素的后面
+    - unique_copy 拷贝区间到另一个区间 连续相等的元素 只拷贝第一个到目标区间(可自定义比较器)
+- 算法复杂度o(n)
 
+//remove例子
+remove.cpp
+*/
+
+/*变序算法
+- 变序算法改变容器中元素的顺序
+- 但是不改变元素的值
+- 变序算法不适用于关联容器
+- 算法复杂度都是o（n）
+- 常见变序算法
+    - reverse 颠倒区间的前后次序
+        template<class BidIt>
+        void reverse(BidIt first,BidIt last);
+        颠倒区间[first,last)顺序
+    - reverse_copy 把一个区间颠倒后的结果拷贝到另一个区间
+    - rotate 将区间进行循环左移
+    - rotate_copy 将区间以首尾相接的形式进行旋转后的结果拷贝到另一个区间，源区间不变
+    - next_permutation 将区间改为下一个排序
+        template<class InIt>
+        bool next_permulation (Init first,Init last);
+        求下一个排列
+    - prev_permutation 将区间改为上一个排序
+    - random_shuffle 随机打乱区间内元素的顺序
+        template<class RanIt>
+        void random_shuffle(RanIt first,RanIt last);
+        随机打乱[first,last)中的元素，适用于能随机访问的容器
+    - partition 把区间内满足某个条件的元素移到全面，不满足该条件移到后面
+        - stable_patiton
+            - 把区间内满足某个条件的元素移到前面
+            - 不满足该条件的移到后面
+            - 而对这两部分元素，分别保持他们原来的先后次序不变
+    
+//next_permutation案例
+next_permutation1.cpp
+next_permutation2.cpp
+*/
+
+/*排序算法
+- 比前面的变序算法复杂度更高，一般是nlogn
+- 排序算法需要随机访问迭代器的支持
+- 不适用于关联容器和list
+- 常用排序算法
+    - sort 将区间从小到大排序(可自定义比较器)
+        - 原理：2套函数模板
+            template<class RanIt>
+            void sort(RanIt first,RanIt last);
+            按升序排序
+            判断x是否应比y靠前，就看x<y是否为true
+            template<class RanIt,class Pred>
+            void sort(RanIt first,RanIt last,Pred pr);
+            按升序排序
+            判断x是否应比y靠前，就看pr(x,y)是否为true
+        - sort实际上是快速排序 复杂度nlogn
+            - 平均性能最优
+            - 但是最坏的情况下，性能可能非常差
+        - 如果要保证‘最坏情况下’的性能，那么可以使用
+            - stable_sort
+            - stable_sort实际上是归并排序，特点是能保持相等元素之间的先后次序
+            - 在有足够存储空间的情况下，复杂度为nlogn，否则复杂度为nlognlogn
+            - stable_sort用法和sort相同
+        - 排序算法要求随机存取迭代器的支持，所以list不能使用排序算法，要使用list::sort
+
+    - stable_sort 将区间从小到大排序 并保持相等元素间的相对次序(可自定义比较器)
+    - partial_sort 对区间部分排序 直到最小的n个元素就位(可自定义比较器)
+    - partial_sort_copy 将区间前n个元素的排序结果拷贝到别处 源区间不变(可自定义比较器)
+    - nth_element 对区间部分排序 使得第n小的元素(n从0开始算)就位 而且比它小的都在它前面
+                    比它大的都在它后面(可自定义比较器)
+    - make_heap 使区间成为一个‘堆’(可自定义比较器)
+    - push_heap 将元素加入一个是‘堆’区间(可自定义比较器)
+    - pop_heap 从‘堆’区间删除堆顶元素(可自定义比较器)
+    - sor_heap 将一个‘堆’区间进行排序，排序结束后，该区间就是普通的有序区间，
+                不再是‘堆’了(可自定义比较器)
+*/
+
+/*有序区间算法
+- 要求所操作的区间是已经从小到大排好序的
+- 需要随机访问迭代器的支持
+- 有序区间算法不能用于关联容器和list
+- 此类常见算法
+    - binary_search 判断区间种是否包含某个元素
+        - 折半查找
+        - 要求容器已经有序且支持随机访问迭代器，返回是否找到
+        template<class FwdIt,class T>
+        bool binary_search(FwdIt first,FwdIt last,const T & val);
+        - 上面这个版本，比较两个元素x,y大小时，看x<y
+        template<class FwdIt,class T,class Pred>
+        bool binary_search(FwdIt first,FwdIt last,const T & val,Pred pr);
+        - 上面这个版本，比较两个元素x,y大小时，若pr(x,y)为true，则认为x小于y
+
+    - includes 判断是否一个区间种的每个元素，都在另一个区间中
+        template<class InIt1,class InIt2>
+        bool includes(InIt1 first1,InIt1 last1,InIt2 first2,InIt2 last2);
+        template<class InIt1,class InIt2,class Pred>
+        bool includes(InIt1 first1,InIt1 last1,InIt2 first2,InIt2 last2,Pred pr);
+        - 判断[first2,last2)中的每个元素，是否都在[first1,last1)中
+            - 第一个用<作比较器
+            - 第二个用pr作比较器，pr(x,y) == true说明x，y相等
+    - lower_bound 查找最后一个不小于某值的元素的位置
+        template<class FwdIt,class T>
+        FwdIt lower_bound(FwdIt first,FwdIt last,const T&val);
+        - 要求[first,last)是有序的
+        - 查找[first,last)中的，最大的位置FwdIt,使得[first,FwdIt)中所有元素都比val小
+    - upper_bound 查找第一个大于某值的元素的位置
+        template<class FwdIt,class T>
+        FwdIt upper_bound(FwdIt first,FwdIt last,const T&val);
+        - 要求[first,last)是有序的
+        - 查找[first,last)中的，最小的位置FwdIt,使得[first,FwdIt)中所有元素都比val大
+    - equal_range 同时获取lower_bound和upper_bound
+        template<class FwdIt,class T>
+        pair<FwdIt,FwdIt>equal_range(FwdIt first,FwdIt last,const T&val);
+        - 要求[first,last)是有序的
+        - 返回值是一个pair，假设为p，则：
+            - [first,p.first)中的元素都比val小
+            - [p.second,last)中的元素都比val大
+            - p.first就是lower_bound的结果
+            - p.last就是upper_bound的结果
+    - merge 合并两个有序区间到第三个区间
+        template<class InIt1,class InIt2,class OutIt>
+        OutIt merge(InIt1 first1,InIt1 last1,InIt2 first2,InIt2 last2,OutIt x);
+        - 用<作比较器
+        template<class InIt1,class InIt2,class OutIt，class Pred>
+        OutIt merge(InIt1 first1,InIt1 last1,InIt2 first2,InIt2 last2,OutIt x,Pred pr);
+        - 用pr作比较器
+            - 把[first1,last1),[first2,last2)两个升序序列合并，
+              形成第3个升序序列，第3个升序序列以x开头
+    - set_union 将两个有序区间的并拷贝到第三个区间
+        template<class InIt1,class InIt2,class OutIt>
+        OutIt set_union(InIt1 first1,InIt1 last1,InIt2 first2,InIt2 last2,OutIt x);
+        - 用<比较器
+        template<class InIt1,class InIt2,class OutIt,class Pred>
+        OutIt set_union(InIt1 first1,InIt1 last1,InIt2 first2,InIt2 last2,OutIt x,Pred pr);  
+        - 用pr(x,y)比较
+        -求两个区间的并，放到以x开始的位置
+        - 若某个元素e在[first1,last1)里出现n1次，在[first2,last2)里出现n2次，则该元素在目标区间里出现max(n1,n2)次
+    - set_intersection 将两个有序区间的交拷贝到第三个区间
+        template<class InIt1,class InIt2,class OutIt>
+        OutIt set_intersection(InIt1 first1,InIt1 last1,InIt2 first2,InIt2 last2,OutIt x);
+        template<class InIt1,class InIt2,class OutIt,class Pred>
+        OutIt set_intersection(InIt1 first1,InIt1 last1,InIt2 first2,InIt2 last2,OutIt x,Pred pr);
+        - 求出[first1,last1)和[first2,last2)中共有的元素，放到从x开始的地方
+        - 若某个元素e在[first1,last1)里出现n1次，在[first2,last2)里出现n2次，则该元素在目标区间里出现min(n1,n2)次
+    - set_difference 将两个有序区间的差拷贝到第三个区间
+        template<class InIt1,class InIt2,class OutIt>
+        OutIt set_difference(InIt1 first1,InIt1 last1,InIt2 first2,InIt2 last2,OutIt x);
+        template<class InIt1,class InIt2,class OutIt,class Pred>
+        OutIt set_difference(InIt1 first1,InIt1 last1,InIt2 first2,InIt2 last2,OutIt x,Pred pr);
+        - 求出[first1,last1)中，不在[first2,last2)中的元素，放到从x开始的地方
+        - 如果[first1,last1)里有多个相等元素不在[first2,last2)中，则这多个元素也都会被放入x代表的目标区间里
+    - set_symmetric_difference 将两个有序区间的对称差拷贝到第三个区间
+        template<class InIt1,class InIt2,class OutIt>
+        OutIt set_symmetric_difference(InIt1 first1,InIt1 last1,InIt2 first2,InIt2 last2,OutIt x);
+        template<class InIt1,class InIt2,class OutIt,class Pred>
+        OutIt set_symmetric_difference(InIt1 first1,InIt1 last1,InIt2 first2,InIt2 last2,OutIt x,Pred pr);
+        - 把两个区间里相互不在另一区间里的元素放入x开始的地方
+    - inplace_merge  将两个连续的有序区间原地合并为一个有序区间
+
+*/
+
+
+/*bitset类模板
+template <size_t N>
+class bitset{
+    ...
+};
+- 实际使用的时候，N是个整型常数
+- 如：
+    - bitset<40> bst;
+    - bst是一个由40位组成的对象
+    - 用bitset的函数可以方便地访问任何一位
+- bitset的成员函数
+    - bitset<N>&operator&=(const bitset<N>& rhs);
+    - bitset<N>&operator|=(const bitset<N>& rhs);
+    - bitset<N>&operator^=(const bitset<N>& rhs);
+    - bitset<N>&operator<<=(size_t num);
+    - bitset<N>&operator>>=(size_t num);
+    - bitset<N>&set();//全部设成1
+    - bitset<N>&set(size_t pos,bool val =true);//设置某位
+    - bitset<N>&reset();//全部设成0
+    - bitset<N>&reset(size_t pos);//某位设成0
+    - bitset<N>&flip();//全部翻转
+    - bitset<N>&flip(size_t pos);//翻转某位
+
+    - reference operator[](size_t pos);//返回对某位的引用
+    - bool operator[](size_t pos)const;//判断某位是否为1
+    - reference at(size_t pos);
+    - bool at(size_t pos)const;
+    - unsigned long to_ulong()const;//转换成整数
+    - string to_string()const;//转换成字符串
+    - size_t count()const;//计算1的个数
+    - size_t size()const;
+    - bool operator==(const bitset<N>&rhs)const;
+    - bool operator!=(const bitset<N>&rhs)const;
+
+    - bool test(size_t pos) const;//测试某位是否为1
+    - bool any() const;//是否由某位为1
+    - bool none() const;//是否全部为0
+    - bitset<N> operator<<(size_t pos) const;
+    - bitset<N> operator>>(size_t pos) const;
+    - bitset<N> operator~();
+    - static const size_t bitset_size = N;
+    //注意第0位在最右边
+*/
